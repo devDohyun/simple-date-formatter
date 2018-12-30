4ff29027b64f0aeea96b6ceb490e8db7d626d6ff
devDohyun <40540101+devDohyun@users.noreply.github.com>
2018-12-30 18:22:12 +0900

    Date Format changed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /*
+    v0.0.2
     2018. 12. 30.
     Author : Dohyun Ahn
     git : https://github.com/devDohyun/simple-date-formatter
@@ -6,12 +7,12 @@
     thankyou for use my code
 */
 
-function dateFormatter (format, lang = 'kor') {
+function dateFormatter (format, lang = 'eng') {
     let obj = this
     let result = ''
 
     const dayNamesKor = ['일', '월', '화', '수', '목', '금', '토']
-    const dayNamesEng = ['SUN', 'MON', 'TUE', 'WEN', 'THU', 'FRI', 'SAT']
+    const dayNamesEng = ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat']
 
     const periodKor = {'am': '오전', 'pm': '오후'}
     const periodEng = {'am': 'am', 'pm': 'pm'}
@@ -21,22 +22,31 @@ function dateFormatter (format, lang = 'kor') {
 
     const setDigit = (val, digit = 2) => { if (val.toString().length < digit) while (val.toString().length < digit) val = '0' + val; return val}
 
-    result = format.replace(/(Y|yy|M|ddd|d|D|HH|H|hh|h|mm|m|s|a)/g, ($1) => {
+    result = format.replace(/(yyyy|yy|MM|M|ddd|dd|d|HH|H|hh|h|mm|m|ss|s|tt)/g, ($1) => {
         switch ($1) {
-            case 'Y': return obj.getFullYear()
+            case 'yyyy': return obj.getFullYear()
             case 'yy': return obj.getFullYear() % 100
-            case 'MM': return obj.getMonth() + 1
+
+            case 'MM': return setDigit(obj.getMonth() + 1)
             case 'M': return obj.getMonth() + 1
-            case 'D': return obj.getDate()
+
             case 'ddd': return dayNames[obj.getDay()]
-            case 'H': return obj.getHours()
+            case 'dd': return setDigit(obj.getDate())
+            case 'd': return obj.getDate()
+            
             case 'HH': return setDigit(obj.getHours())
-            case 'h': return obj.getHours() % 12
+            case 'H': return obj.getHours()
+            
             case 'hh': return setDigit(obj.getHours() % 12)
-            case 'm': return obj.getMinutes()
+            case 'h': return obj.getHours() % 12
+
             case 'mm': return setDigit(obj.getMinutes())
+            case 'm': return obj.getMinutes()
+
+            case 'ss': return setDigit(obj.getSeconds())
             case 's': return obj.getSeconds()
-            case 'a': return obj.getHours < 12 ? period['am'] : period['pm']
+
+            case 'tt': return obj.getHours < 12 ? period['am'] : period['pm']
             default: return $1
         }
     })
